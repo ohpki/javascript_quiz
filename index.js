bef4980cd98c4a900aa6a4ab0ab6254122646162
希 <98795405+ohpki@users.noreply.github.com>
2022-05-19 21:34:25 +0900

    修正

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const quiz = [
   {
     question: "魚のタラは漢字で書くと魚へんに何と書く？",
-    answers: [ "占", "念", "雪", "寒" ],
+    answers: [ "占", "周", "雪", "寒" ],
     correct: "雪"
   },{
     question: "突発的にキーンと高音の耳鳴りがする原因は？",
@@ -60,3 +60,47 @@ while (handlerIndex < buttonLength) {
   });
   handlerIndex++;
 }
+
+
+
+// let mes = 'Hello';
+
+// const mess = 'hey';
+// let inoki = ['いち', 'に', 'さん']
+// let six = ['1', '2', '3', '4', '5', '6', '7']
+// //let index = 0;
+// //while(index < inoki.length){
+// //  console.log(inoki[index]);
+// //  index++;
+  
+// //}
+
+// // if(inoki.length > 5){
+// //   console.log("woo!")
+
+// // } else {
+// //   console.log("!")
+// // }
+
+// //関数　同じ命令を何度も使いたい　特定の条件で使いたい
+// const test = (arg) => {
+//   if(arg.length > 5){
+//     console.log("woo!")
+  
+//   } else {
+//     console.log("!")
+//   }
+// };
+// test(inoki);
+
+// //オブジェクト
+// // const cat = {
+// //   color: 'pink'
+// //   size: 'big'
+// //   purfume: 'mint'
+// // };
+
+
+// console.log(document.getElementsByTagName("button")[0].addEventListener("click", ()=> {
+//   window.alert("hey");
+// }));
